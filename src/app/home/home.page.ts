755ec718bdfdc07cbf2ae9f8517095755e8ca7a9
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2022-04-25 14:33:46 +0200

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
+  @ViewChild('videoElementLocal') videoElementLocal: ElementRef;
   constructor() {}
+
+  async ngOnInit() {
+
+    const localElement = this.videoElementLocal.nativeElement;
+
+    const  localStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+
+
+    localElement.srcObject = localStream;
+  }
 
 }
